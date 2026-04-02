8207911ba4c8fd51a0c31e817feefdaecd3da619
Nurzhan <nurzhanbakyt04@gmail.com>
2026-04-02 17:39:47 +0600

    1 dz

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import Text from "./components/Text";
+
+function App() {
+  return <div>
+    <h2>Hello world</h2>
+    <Text/>
+  </div>;
+}
+export default App
---
+++ src/components/Text.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+const Text = () => {
+  return (
+    <div className="divWrap">
+      <div className="div">
+        <img
+          src="https://ps.w.org/simple-user-avatar/assets/icon-256x256.png?rev=2413146"
+          alt=""/>
+      </div>
+       <div className="div">
+        <img
+          src="https://ps.w.org/simple-user-avatar/assets/icon-256x256.png?rev=2413146"
+          alt=""/>
+      </div>
+       <div className="div">
+        <img
+          src="https://ps.w.org/simple-user-avatar/assets/icon-256x256.png?rev=2413146"
+          alt="" />
+      </div>
+       <div className="div">
+        <img
+          src="https://ps.w.org/simple-user-avatar/assets/icon-256x256.png?rev=2413146"
+          alt=""/>
+      </div>
+       <div className="div">
+        <img
+          src="https://ps.w.org/simple-user-avatar/assets/icon-256x256.png?rev=2413146"
+          alt=""/>
+      </div>
+     
+      
+    </div>
+  );
+};
+
+export default Text;
